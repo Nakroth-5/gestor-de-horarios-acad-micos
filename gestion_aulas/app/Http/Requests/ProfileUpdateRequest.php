749b6cf1ab0d250452c7b16cfe20d559db5faa4c
Evert <evert.aov@gmail.com>
2025-10-26 10:34:47 -0400

    sulucion de error de todo

---
+++ gestion_aulas/app/Http/Requests/ProfileUpdateRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use App\Models\User;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class ProfileUpdateRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            // Reglas actualizadas y nuevas
+            'name' => ['required', 'string', 'max:50'],
+            'last_name' => ['required', 'string', 'max:100'],
+            'phone' => ['nullable', 'string', 'max:20'],
+            'address' => ['nullable', 'string', 'max:500'],
+            'email' => [
+                'required',
+                'string',
+                'lowercase',
+                'email',
+                'max:50', // Límite actualizado
+                Rule::unique(User::class)->ignore($this->user()->id),
+            ],
+            'document_type' => ['required', 'string', Rule::in(['CI', 'PASSPORT'])],
+            'document_number' => ['required', 'integer', 'digits_between:5,12'],
+        ];
+    }
+}
