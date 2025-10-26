672df9841da4c3e6ada46cad5d315dce9529bd8c
Evert <evert.aov@gmail.com>
2025-10-26 10:52:08 -0400

    correccion de rutas

--- gestion_aulas/routes/web.php
+++ gestion_aulas/routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
-use app\Livewire\RoleManager;
-use app\Livewire\UserManager;
+use App\Livewire\RoleManager;
+use App\Livewire\UserManager;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
