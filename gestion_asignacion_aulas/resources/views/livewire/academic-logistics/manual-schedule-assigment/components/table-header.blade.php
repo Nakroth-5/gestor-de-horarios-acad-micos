8396a353d55cfd06db538077f18fe1368321c30b
Evert <evert.aov@gmail.com>
2025-11-08 15:23:23 -0400

    Reestructuración de la cabecera de la tabla en ManualScheduleAssignment para optimizar generación dinámica y consistencia en los encabezados.

--- gestion_asignacion_aulas/resources/views/livewire/academic-logistics/manual-schedule-assigment/components/table-header.blade.php
+++ gestion_asignacion_aulas/resources/views/livewire/academic-logistics/manual-schedule-assigment/components/table-header.blade.php
@@ -23,39 +23,21 @@
         </div>
     </x-table-header>
 
-    <x-table-header>
-        <div class="flex items-center space-x-2">
-            <x-icons.timedate class="w-4 h-4" /> <span>{{ __('DIA') }}</span>
-        </div>
-    </x-table-header>
+    @for($i = 1; $i <= 6; $i++)
+        <x-table-header>
+            <div class="flex items-center space-x-2">
+                <x-icons.timedate class="w-4 h-4" /> <span>{{ __('CLASSROOM/DAY') }}</span>
+            </div>
+        </x-table-header>
+
+        <x-table-header>
+            <div class="flex items-center space-x-2">
+                <x-icons.time class="w-4 h-4" /> <span>{{ __('HOUR') }}</span>
+            </div>
+        </x-table-header>
+    @endfor
 
     <x-table-header>
-        <div class="flex items-center space-x-2">
-            <x-icons.time class="w-4 h-4" /> <span>{{ __('HORA') }}</span>
-        </div>
-    </x-table-header>
-
-    <x-table-header>
-        <div class="flex items-center space-x-2">
-            <x-icons.timedate class="w-4 h-4" /> <span>{{ __('DIA') }}</span>
-        </div>
-    </x-table-header>
-
-    <x-table-header>
-        <div class="flex items-center space-x-2">
-            <x-icons.time class="w-4 h-4" /> <span>{{ __('HORA') }}</span>
-        </div>
-    </x-table-header>
-
-    <x-table-header>
-        <div class="flex items-center space-x-2">
-            <x-icons.timedate class="w-4 h-4" /> <span>{{ __('DIA') }}</span>
-        </div>
-    </x-table-header>
-
-    <x-table-header>
-        <div class="flex items-center space-x-2">
-            <x-icons.time class="w-4 h-4" /> <span>{{ __('HORA') }}</span>
-        </div>
+        <x-icons.settings/> <span>{{ __('OPCIONES') }}</span>
     </x-table-header>
 </div>
