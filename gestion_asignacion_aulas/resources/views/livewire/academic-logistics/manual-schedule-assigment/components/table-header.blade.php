d63c9b54e20b70ac8b21b51b6f77c68fe86bf2a4
Evert <evert.aov@gmail.com>
2025-11-10 18:51:19 -0400

    En produccion

--- gestion_asignacion_aulas/resources/views/livewire/academic-logistics/manual-schedule-assigment/components/table-header.blade.php
+++ gestion_asignacion_aulas/resources/views/livewire/academic-logistics/manual-schedule-assigment/components/table-header.blade.php
@@ -23,21 +23,18 @@
         </div>
     </x-table-header>
 
-    @for($i = 1; $i <= 6; $i++)
+    {{-- 3 columnas para horarios con aulas --}}
+    @for($i = 1; $i <= 3; $i++)
         <x-table-header>
             <div class="flex items-center space-x-2">
-                <x-icons.timedate class="w-4 h-4" /> <span>{{ __('CLASSROOM/DAY') }}</span>
+                <x-icons.timedate class="w-4 h-4" /> <span>{{ __('DIA') }} {{ $i }}</span>
             </div>
         </x-table-header>
 
         <x-table-header>
             <div class="flex items-center space-x-2">
-                <x-icons.time class="w-4 h-4" /> <span>{{ __('HOUR') }}</span>
+                <x-icons.time class="w-4 h-4" /> <span>{{ __('HORA/AULA') }} {{ $i }}</span>
             </div>
         </x-table-header>
     @endfor
-
-    <x-table-header>
-        <x-icons.settings/> <span>{{ __('OPCIONES') }}</span>
-    </x-table-header>
 </div>
