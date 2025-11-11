1aad88cbc04f2714c5245c070b8a52e300540f0c
Evert <evert.aov@gmail.com>
2025-11-11 08:18:08 -0400

    Simplificación y actualización de estilos en `select-input`: ajustes en clases CSS para mejorar consistencia visual y reducir redundancia.

--- gestion_asignacion_aulas/resources/views/components/select-input.blade.php
+++ gestion_asignacion_aulas/resources/views/components/select-input.blade.php
@@ -1,18 +1,9 @@
 @props(['disabled' => false])
 
-<select 
-    @disabled($disabled) 
+<select
+    @disabled($disabled)
     {{ $attributes->merge([
-        'class' => 'mt-2 block w-full
-                    bg-white dark:bg-gray-800
-                    border border-gray-300 dark:border-gray-700
-                    text-gray-900 dark:text-white
-                    focus:border-indigo-500 focus:ring-2 focus:ring-indigo-500 focus:ring-offset-0 dark:focus:ring-offset-gray-800
-                    rounded-md shadow-sm
-                    cursor-pointer
-                    transition-colors duration-150
-                    hover:bg-gray-50 dark:hover:bg-gray-700
-                    disabled:opacity-50 disabled:cursor-not-allowed
+        'class' => 'mt-1 block w-full border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm
                     [&>option]:bg-white dark:[&>option]:bg-gray-800
                     [&>option]:text-gray-900 dark:[&>option]:text-white
                     [&>option]:py-2'
